8bd8e6ed8a58d9d84da09006effc618217783793
kafkajing <kafkajing>
2018-07-19 21:49:19 +0800

    修改了个群名字

--- AI.py
+++ AI.py
@@ -4,7 +4,7 @@ pag.FAILSAFE = True
 print('程序会在三秒后开始运行，现在快去打开微信并把它全屏。')
 time.sleep(3)
 qun_num=[16]
-qunlist = ['冷笑话']
+qunlist = ['阅读群']
 # for z in range(len(qun_num)):
 #     qunlist.append('摄影入门【38期'+str(qun_num[z])+'班】')
 for x in qunlist:
